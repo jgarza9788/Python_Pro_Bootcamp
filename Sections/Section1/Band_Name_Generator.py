b846dc904e1c1567476c792fe51042c1d0080959
jgarza9788 <jgarza9788@gmail.com>
2022-06-05 16:09:38 -0400

    first project - band name generator
    it's basically a hello world :P

---
+++ Sections/Section1/Band_Name_Generator.py
@@ -0,0 +1,9 @@
+
+# creating a band name generator
+# https://replit.com/@appbrewery/band-name-generator-end
+
+
+print('Welcome to the Band Name Generator.')
+city_name = input('What\'s the name of the city you group up in?\n')
+pet_name = input('What\'s your pet\'s name?\n')
+print('Your band name could be {city_name} {pet_name}'.format(city_name=city_name,pet_name=pet_name))
