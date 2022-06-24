47f09a2fd36d9a6e24fe01eebd53e36a15f4aed7
jgarza9788 <jgarza9788@gmail.com>
2022-06-23 20:02:23 -0400

    day 32

---
+++ Sections/Section32_bday_email/main.py
@@ -0,0 +1,42 @@
+import smtplib
+
+
+"""
+google no longer supports this...
+>skipping until i really need to do this.
+"""
+# my_email = "pythontu59@gmail.com"
+# password = 'almost_password'
+
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls("")
+#     connection.login(user=my_email,password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addr="pythontu59@yahoo.com",
+#         msg='what up?'
+#         )
+#     # connection.close()
+
+
+"""
+yahoo no longer supports creating a new app password
+>skipping until i really need to do this.
+
+note: selenium
+"""
+# my_email = "pythontu59@yahool.com"
+# password = 'almost_password'
+
+# with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+#     connection.starttls("")
+#     connection.login(user=my_email,password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addr="pythontu59@gmail.com",
+#         msg='what up?'
+#         )
+#     # connection.close()
+
+
+
