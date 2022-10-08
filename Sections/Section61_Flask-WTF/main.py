f7867c15819eeb770f528dc6cc5c0df77ff5fbd8
Justin Garza <jgarza9788@gmail.com>
2022-10-08 18:52:48 -0400

    20221008

---
+++ Sections/Section61_Flask-WTF/main.py
@@ -0,0 +1,43 @@
+from wsgiref.validate import validator
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+
+
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+    return app
+
+# app = Flask(__name__)
+app = create_app()
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        # print(login_form.email.data)
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
