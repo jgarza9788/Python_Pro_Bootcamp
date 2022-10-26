67e050c362e6b14f5df2bfcb80b311fdd270f5e3
jgarza9788 <jgarza9788@gmail.com>
2022-10-26 19:48:32 -0400

    20221026

--- Sections/Section91_color_palette_gen/main.py
+++ Sections/Section91_color_palette_gen/main.py
@@ -1,11 +1,16 @@
 
 
+from ast import Lambda
 from xml.etree.ElementTree import tostring
 from flask import Flask, flash, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 
 import colorgram
+import numpy as np
+
+
+import shrinker as shr
 
 # UI/CSS
 from flask_bootstrap import Bootstrap
@@ -31,6 +36,9 @@ def rgb_to_hex(rgb):
     return '%02x%02x%02x' % rgb
 # rgb_to_hex((255, 255, 195))
 
+
+
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
 
@@ -54,31 +62,56 @@ def home():
 
             # Extract 6 colors from an image.
             fullfilename = os.path.join(UPLOAD_FOLDER,filename)
-            temp = colorgram.extract(fullfilename, 10)
+
+            small_file = shr.downres(fullfilename)
+            print(small_file)
+            
+            temp = colorgram.extract(small_file, 10)
             print(*temp)
 
             colors = []
             for c in temp:
                 # print(c)
-                print(rgb_to_hex(c.rgb))
-                rgb = str([c.rgb[0],c.rgb[1],c.rgb[2]])
+                # print(rgb_to_hex(c.rgb))
 
+                rgb = str([c.rgb[0],c.rgb[1],c.rgb[2]])
+                mean = round(((c.rgb[0]+c.rgb[1]+c.rgb[2])/3.0)/255,2)
+                # sat = round(np.std([c.rgb[0],c.rgb[1],c.rgb[2]]),2)
+
+                sat = round((max([c.rgb[0],c.rgb[1],c.rgb[2]]) - min([c.rgb[0],c.rgb[1],c.rgb[2]]))/255,2)
 
                 txtcolor = '#f8f9fa'
-                print((c.rgb[0] + c.rgb[1] + c.rgb[2])/3.0)
-                if (c.rgb[0] + c.rgb[1] + c.rgb[2])/3.0 > 191:
+                # print((c.rgb[0] + c.rgb[1] + c.rgb[2])/3.0)
+                if mean > 0.5:
                     txtcolor = '#212529'
 
                 colors.append({ 
                     'hex' : '#'+ str(rgb_to_hex(c.rgb)) , 
                     'rgb': rgb,
+                    'mean': mean,
+                    'sat': sat,
                     'txtcolor': txtcolor,
                     'percent':'{:.2f}'.format(c.proportion*100) 
                     })
 
             print(*colors,sep='\n')
 
-            return render_template('index.html', filename=filename, colors=colors)
+            colors_by_percent = colors[:5]
+            dark = sorted(colors, key=lambda x : x['mean'], reverse=False)[:5]
+            light = sorted(colors, key=lambda x : x['mean'], reverse=True)[:5]
+
+            high_sat = sorted(colors, key=lambda x : x['sat'], reverse=True)[:5]
+            low_sat = sorted(colors, key=lambda x : x['sat'], reverse=False)[:5]
+
+            return render_template(
+                'index.html', 
+                filename=filename, 
+                colors_by_percent = colors_by_percent,
+                dark = dark,
+                light = light,
+                high_sat = high_sat,
+                low_sat = low_sat
+                )
         else:
             flash('Allowed image types are - png, jpg, jpeg, gif')
             print('Allowed image types are - png, jpg, jpeg, gif')
---
+++ Sections/Section92_custom_web_scraper/main.py
@@ -0,0 +1,86 @@
+# this will print out the Trash/Recycling Schedule
+# web scrapped from the Charlotte City Website
+
+import os,sys,re
+
+from seleniumwire import webdriver 
+from selenium.webdriver.common.by import By
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+
+months = ['Jan','Feb','Mar','Apr','May','Jun','July','Aug','Sep','Oct','Nov','Dec']
+
+def get_browser(visible= True):
+    #options
+    options = webdriver.ChromeOptions()
+    options.add_argument('test-type')
+    # options.add_argument("--no-sandbox") # no sandboxing
+    # chrome_options.add_argument("--load-extension=" + unpackedExtensionPath)
+    options.add_argument('--js-flags=--expose-gc')
+    options.add_argument('--enable-precise-memory-info')
+    options.add_argument('--disable-popups-blocking')
+    options.add_argument('--disable-default-apps')
+    options.add_argument('test-type=browser')
+    options.add_argument('disable-infobars')
+    # options.add_argument('window-size=800x600')
+    # options.add_argument('window-size=1600x1200')
+    options.add_argument('start-maximized')
+    # options.add_argument('--kiosk')
+    options.add_argument('log-level=3')
+    # options.add_argument("user-data-dir=C:\\Users\\JGarza\\AppData\\Local\\Google\\Chrome\\User Data\\Default") 
+
+    if visible == False:
+        options.add_argument('headless')
+    
+    return webdriver.Chrome(executable_path=os.path.join(DIR,'chromedriver.exe'),options=options)
+
+def clean_text(t):
+    t = t.lstrip().rstrip()
+    t = re.sub(r'(&nbsp;|Week|-|\.)',' ',t)
+    t = re.sub(r'\s{1,100}',' ',t)
+    t = re.sub(r'(<em>|</em>|\*)','',t)
+    return t.split(' ')
+
+def main():
+    global months
+
+    temp = []
+    result = []
+
+    #this is my browser 
+    browser =  get_browser(False)
+
+    #go to website 
+    url = 'https://charlottenc.gov/SWS/Pages/Recycling-Collection-Schedule.aspx'
+    browser.get(url)
+
+    #find the weeks
+    w = browser.find_elements(By.CLASS_NAME,'swscalpn')
+
+    # clean each text and put it in result
+    for i in w:
+        temp.append(clean_text(i.get_attribute("innerHTML")))
+
+    #find those other weeks
+    w = browser.find_elements(By.CLASS_NAME,'swscalpb')
+
+    # clean each text and put it in result
+    for i in w:
+        temp.append(clean_text(i.get_attribute("innerHTML")))
+
+
+    temp = sorted(temp, key=lambda x: int(x[2]))
+
+    for m in months:
+        for t in temp:
+            # print(m,t[1])
+            if m == t[1]:
+                result.append(t)
+
+    print(*result,sep='\n')
+
+
+
+
+if __name__ == '__main__':
+    main()
